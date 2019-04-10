6855f45ba0351457213e2054b658b0004dba1b07
osysltd <anton@vsprose.ru>
2019-04-10 22:21:09 +0300

    ru-ru cron

---
+++ upload/admin/language/ru-ru/marketplace/cron.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']        = 'Задачи по расписанию';
+
+// Text
+$_['text_success']         = 'Задачи успешно модифицированы!';
+$_['text_instruction']     = 'Инструкции по задачам';
+$_['text_list']            = 'Список задач';
+$_['text_cron_1']          = 'Задачи запускаются с переодичностью. Для настройки прочитайте <a href="http://docs.opencart.com/extension/cron/" target="_blank" class="alert-link">документацию</a>.';
+$_['text_cron_2']          = 'Необходимо установить расписание запуска ежечасно';
+$_['text_hour']            = 'Чвс';
+$_['text_day']             = 'День';
+$_['text_month']           = 'Месяц';
+
+// Column
+$_['column_code']          = 'Код задачи';
+$_['column_cycle']         = 'Цикл';
+$_['column_status']        = 'Статус';
+$_['column_date_added']    = 'Дата добавления';
+$_['column_date_modified'] = 'Дата изменения';
+$_['column_action']        = 'Действие';
+
+// entry
+$_['entry_cron']           = 'URL Задачи';
+
+// Error
+$_['error_permission']     = 'У Вас нет прав для изменения настроек!';
